98c342b15c244f6c31c7d7e9033d2694a675fbe4
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-06-21 23:57:50 +0100

    add: favicon

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -6,7 +6,7 @@ const NavItems = () => {
     <section className="nav-items">
       <Link to="/" className="link-logo">
         <img src="/assets/icons/logo.svg" alt="Logo" className="size-[30px]" />
-        <h1>Tourvisto</h1>
+        <h1>BookMyTrip</h1>
       </Link>
     </section>
   );
