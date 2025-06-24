ed81001aee0b7051b163416e851951befeae432c
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-06-24 15:03:55 +0100

    import { useState } from react;

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,25 +1,24 @@
 import { MobileSidebar, NavItems } from "components";
-import pkg from '@syncfusion/ej2-react-navigations';
-import { Outlet } from "react-router"; 
-
-const { SidebarComponent } = pkg;
+import { Outlet } from "react-router";
 
 const AdminLayout = () => {
   return (
-    <div className="admin-layout">
-      <MobileSidebar />
+    <div className="flex flex-col lg:flex-row h-screen w-full">
       
+      {/* Mobile Sidebar */}
+      <MobileSidebar />
+
       {/* Desktop Sidebar */}
-      <aside className="w-full max-w-[270px] hidden lg:block">
-        <SidebarComponent width={270} enableGestures={false}>
+      <aside className="w-full max-w-[270px] hidden lg:flex flex-col bg-gray-100 border-r border-gray-300">
+        <div className="h-full overflow-y-auto">
           <NavItems />
-        </SidebarComponent>
+        </div>
       </aside>
 
       {/* Main Content */}
-      <aside className="children">
+      <main className="w-full h-full bg-light-200 pt-12 lg:pt-6 overflow-y-auto">
         <Outlet />
-      </aside>
+      </main>
     </div>
   );
 };
