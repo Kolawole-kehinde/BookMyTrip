5556d49fb8e3537849ef7e29635155050512f5f6
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-07-01 20:31:01 +0100

    create: Trips component

---
+++ app/routes/admin/trips.tsx
@@ -0,0 +1,14 @@
+import { Header } from "components"
+
+const Trips = () => {
+  return (
+    <main className="all-users wrapper">
+          <Header
+            title="Trips"
+            description="View and Edit AI-genrated travel plans"
+          />
+          </main>
+  )
+}
+
+export default Trips
