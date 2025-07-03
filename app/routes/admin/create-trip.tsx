08a2e5ba20203f538253f56dd1540994cdb5a704
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-07-03 14:04:11 +0100

    validate: create trip input fields

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -3,7 +3,7 @@ import { Header } from 'components'
 import { ComboBoxComponent } from '@syncfusion/ej2-react-dropdowns'
 import type { Route } from './+types/create-trip'
 import { comboBoxItems, selectItems } from '~/constants'
-import { formatKey } from 'lib/utils'
+import { cn, formatKey } from 'lib/utils'
 import { Coordinate, LayerDirective, LayersDirective, MapsComponent } from '@syncfusion/ej2-react-maps'
 import { world_map } from '~/constants/world_map'
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons'
@@ -51,7 +51,21 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false)
 
-  con
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+       e.preventDefault()
+       setLoading(true);
+       if(
+        !formData.country ||
+        !formData.travelStyle ||
+        !formData.interest ||
+        !formData.budget ||
+        !formData.groupType
+       ){
+        setError('Please provide values for all fields')
+        setLoading(false)
+        return;
+       }
+  };
 
   // ✅ handleChange keeps formData updated
   const handleChange = (key: keyof TripFormData, value: string | number) => {
@@ -88,7 +102,7 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
         description="View and Edit AI Generated travel plans"
       />
       <section className="mt-2.5 w-full max-w-3xl px-4 lg:px-8 mx-auto">
-        <form className="trip-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="trip-form" onSubmit={handleSubmit}>
           {/* Country */}
           <div>
             <label htmlFor="country" className="block mb-1 font-medium">
@@ -217,7 +231,7 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
                  className='button-class !h-12 !w-full'
                  disabled={loading}
                  >
-                  <img src={`/assets/icons/${loading ? 'loader.svg' : 'magic-star.svg'}`} />
+                  <img src={`/assets/icons/${loading ? 'loader.svg' : 'magic-star.svg'}`}  className={cn('size-5', {'animate-spin': loading})}/>
                       <span className='p-16-semibold text-white'>
                          {loading ? 'Generating' : 'Gnerate Trip' }
                       </span>
